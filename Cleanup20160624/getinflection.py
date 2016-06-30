ee348dc85e7d262d238107abdc82899e8caf5025
tylerlau07 <tyler.lau.07@gmail.com>
2016-06-30 03:40:25 -0400

    Update

--- Cleanup20160624/getinflection.py
+++ Cleanup20160624/getinflection.py
@@ -2,30 +2,158 @@
 
 # Author: Tyler Lau
 # This program will fetch the inflected forms of each word
-
 import urllib
-import re
-import csv
+import unicodecsv as csv
 
-from bs4 import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 import codecs
 
 #############
 # Functions #
 #############
 
-def lookUp(word):
-	soup = urllib.urlopen("https://en.wiktionary.org/wiki/%s" % word)
+# def findLatinSec(word):
+# 	# create a Wiki object
+# 	site = wiki.Wiki("http://en.wiktionary.org/w/api.php") 
+# 	# define the params for the query
+# 	params = {'action':'parse', 'page':word, 'format':'json', 'prop':'sections'}
+# 	# create the request object
+# 	request = api.APIRequest(site, params)
+# 	# query the API
+# 	result = request.query()['parse']['sections']
+# 	# get the section number that says "Latin"
+# 	for i, j in enumerate(result):
+# 		if "Latin" in j["line"]:
+# 			return i + 1
+
+# def getInfl(word, sec):
+# 	# create a Wiki object
+# 	site = wiki.Wiki("http://en.wiktionary.org/w/api.php") 
+# 	# define the params for the query
+# 	params = {'action':'parse', 'page':word, 'format':'json', 'section':sec}
+# 	# create the request object
+# 	request = api.APIRequest(site, params)
+# 	# query the API
+# 	result = request.query()['parse']
+
+# 	parsed_html = BeautifulSoup(result)
+
+# 	print parsed_html
+
+	# request = urllib2.Request('http://en.wiktionary.org/w/api.php', urllib.urlencode({'action':'parse', 'page':word, 'format':'json', 'prop':'sections'}))
+	# request = urllib2.urlopen(request)
+
+	# json_string = "https://en.wiktionary.org/w/api.php?action=parse&page=%s&format=json&prop=sections" % word
+	# parsed_json = json.loads(json_string)
+	# print parsed_json
+
+def InflTable(word):
+	'''
+	Takes in a word and returns the inflection table,
+	whether it is defective (lacking a number),
+	or if there is variation in the individual inflections
+	'''
+	soup = urllib.urlopen('https://en.wiktionary.org/wiki/%s' % word)
 	htmlSource = soup.read()                            
 	soup.close() 
 
 	parsed_html = BeautifulSoup(htmlSource)
-	print parsed_html
+
+	# If there is only one inflection table
+	if len(parsed_html.findAll('table', attrs={'class':'prettytable inflection-table'})) == 1:
+
+		infl_table = parsed_html.find('table', attrs={'class':'prettytable inflection-table'})
+
+		# Check if defective or variable
+		defective = False
+		variation = False
+
+		numbers = infl_table.findAll('th', attrs={'style':'background:#549EA0; font-style:italic;'})
+
+		# If defective, return which number exists
+		if len(numbers) != 3:
+			defective = numbers[1].text
+
+		# Check if variation in forms
+		if len(infl_table.findAll('span')) != 12:
+			variation = True
+
+		return infl_table, defective, variation
+
+	# If multiple inflection tables (because of multiple definitions), easier to just check which is correct
+	else: return None
+
+def getInfl((infl_table, defective, variation)):
+	'''
+	Takes in an inflection table and returns the inflection
+	If defective, will return only singular or plural
+	If there is variation, inflections will be divided by /
+	'''
+	to_return = []
+
+	if defective == False:
+		# Only one form for each inflection
+		if variation == False:
+			to_return += [inflection.text for inflection in infl_table.findAll('td')]
+
+		# Multiple forms for each inflection
+		else:
+			for inflection in infl_table.findAll('td'):
+				# Join multiple inflections with /
+				infl_form = '/'.join([inflection.text for inflection in inflection.findAll('span')])
+				to_return.append(infl_form)
+	
+	# One of the numbers is lacking
+	else:
+		for inflection in infl_table.findAll('td'):
+			# Join multiple inflections with /
+			infl_form = '/'.join([inflection.text for inflection in inflection.findAll('span')])
+
+			if defective == 'Singular':
+				to_return += [infl_form, '-']
+
+			elif defective == 'Plural':
+				to_return += ['-', infl_form]
+
+			else:
+				print "Check what numbers are represented because it is neither Singular nor Plural"
+				return ["CHECK"]
+
+	# Sanity check
+	if len(to_return) == 12:
+		return to_return	
+	else:
+		print "The number of inflections is not 12"
+		return ["CHECK"]
+
+	return to_return
 
 ########
 # Main #
 ########
 
-lookUp("res")
+# print getInfl(InflTable("domus"))
 
-# reader = codecs.open('declinedforms.txt', encoding='utf-8', mode='rU')
+reader = codecs.open('latin_nouns.txt', encoding='utf-8', mode='rU')
+
+with open('noun_paradigms2.txt', mode = 'wb') as f:
+	out = csv.writer(f, delimiter = '\t')
+	out.writerow(["Latin", "English", "Declension", "Gender", "Total", "Prose", "Poetry",
+		"NomSg", "NomPl", "GenSg", "GenPl", "DatSg", "DatPl", "AccSg", "AccPl", "AblSg", "AblPl", "VocSg", "VocPl"])
+
+	for row in reader.readlines()[1:]:
+		row = row.strip("\r\n").split("\t")
+
+		# Create object to be added to
+		to_write = row
+		word = row[0]
+
+		# If there are multiple inflection tables (different definitions/parts of speech), just check and fix manually
+		if InflTable(word) == None:
+			to_write += ["CHECK"]
+
+		else:
+			to_write += getInfl(InflTable(word))
+
+		print word, to_write
+		out.writerow(to_write)
---
+++ Cleanup20160624/getrootstem.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+# Author: Tyler Lau
+# This program will determine the root and case endings of each word
+# We use the lemmatizer at http://www.ilc.cnr.it/lemlat/
+
+import urllib
+import time
+import re
+import unicodecsv as csv
+
+from bs4 import BeautifulSoup
+import codecs
+
+start = time.time()
+
+cases = [
+		'nomsg', 'nompl', 
+		'gensg', 'genpl', 
+		'datsg', 'datpl',
+		'accsg', 'accpl',
+		'ablsg', 'ablpl',
+		'vocsg', 'vocpl'
+		]
+
+LV = {u'ā':'a',u'ē':'e',u'ī':'i',u'ō':'o',u'ū':'u',u'ȳ':'y'}
+
+#############
+# Functions #
+#############
+
+def findIndices(word):
+	'''
+	Finds the indices for the long vowels and v's for replacement to search
+	Returns rewritten word, dictionary mapping LV indices to long vowels, and list of v indices
+	'''
+	LV_indices = {}
+	v_indices = []
+	for i in range(0, len(word)):
+		# Find indices of long vowels and replace with short
+		if word[i] in LV.keys():
+			LV_indices[i] = word[i]
+			word = word[:i] + LV[word[i]] + word[i+1:]
+		# Get v indices
+		elif word[i] == 'v':
+			v_indices.append(i)
+	return word, LV_indices, v_indices
+
+def segmentWord(word):
+	'''
+	Looks up a word in the Latin lemmatizer
+	Returns root and suffix
+	'''
+	soup = urllib.urlopen("http://www.ilc.cnr.it/lemlat/cgi-bin/LemLat_cgi.cgi?World+Form="+word)
+	htmlSource = soup.read()                            
+	soup.close() 
+
+	parsed_html = BeautifulSoup(htmlSource)
+
+	# Create flag for whether segmentation has been found or not
+	segment_found = False
+	relevant = parsed_html.body.find('td', attrs={'valign':'top'})
+
+	# Search through "analyses" until proper segmentation is found
+	while segment_found == False:
+		# If there is a root and suffix(es)
+		if len(re.findall("\S+", relevant.text)) >= 2:
+			segment_found = True
+		# Otherwise, find the next one
+		else:
+			# Search next for analysis
+			if relevant.find_next('td', attrs={'valign':'top'}) != None:
+				relevant = relevant.find_next('td', attrs={'valign':'top'})
+			# If out of analyses, determine the root is null
+			else: 
+				segment_found = True
+
+	root_suf = re.findall("\S+", relevant.text)
+
+	root = root_suf[0]
+
+	# Collapse if multiple suffixes
+	if len(root_suf) >= 2:
+		suf = ''.join(root_suf[1:]).strip('-')
+
+	# Suffix null if only 1
+	else:
+		suf = 'NULL'
+
+	return root, suf
+
+def restoreLVV(root, suffix, LV_indices, v_indices):
+	'''
+	Re-replace vowels with long vowels in root/suffix
+	Takes in the root, suffix, and dictionary and list of indices
+	'''
+	for index, vowel in LV_indices.iteritems():
+		# If index smaller than length of root, then in root
+		if index < len(root):
+			root = root[:index] + vowel + root[index+1:]
+		# Else it is in the suffix
+		else:
+			suf_index = index - len(root)
+			suffix = suffix[:suf_index] + vowel + suffix[suf_index+1:]
+
+	# Re-replace 'u' with 'v'
+	for index in v_indices:
+		root = root[:index] + 'v' + root[index+1:]
+
+	return root, suffix
+
+########
+# Main #
+########
+
+# print segmentWord('orientes')
+
+# _cont if continuing because of timeout
+reader = codecs.open('noun_paradigms_cont.txt', encoding='utf-8', mode='rU')
+
+with open('rootending.txt', mode = 'ab') as f:
+	out = csv.writer(f, delimiter = '\t')
+	# Don't write following if continuing
+	# out.writerow(["Form", "Root", "Suffix", "Case", "Gender", "Declension", "Parent", "English", "Total", "Prose", "Poetry"])
+	# Each row is a word's case forms
+	for row in reader.readlines()[1:]:
+		row = row.strip("\r\n").split("\t")
+		# First and second elements are gender and declension
+		lat, eng, dec, gender, total, prose, poetry = row[0:7]
+		
+		# Each element in the row is a word
+		for i in range(7, len(row)):
+			if "/" in row[i]:
+				word = row[i][:row[i].index("/")]
+			else:
+				word = row[i]
+			case = cases[i-7]
+
+			try:
+				searchword, LV_indices, v_indices = findIndices(word)
+				root, suffix = segmentWord(searchword)
+				re_root, re_suffix = restoreLVV(root, suffix, LV_indices, v_indices)
+
+			# If there is no breakdown
+			except AttributeError:
+				re_root, re_suffix = ('N/A', 'N/A')
+
+			# Write out the word and case
+			to_write = word, re_root, re_suffix, case, gender, dec, lat, eng, total, prose, poetry
+			print word, to_write
+			out.writerow(to_write)
+		
